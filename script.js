7eb2c96383d0d69e89ace3bf8f0faed498cd6947
ILikeMonday <90751276+ILikeMonday@users.noreply.github.com>
2022-08-04 14:19:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const inputface = document.getElementById("img-profile");
+const inputname = document.getElementById("#p-name");
+const inputemail = document.getElementById("#p-email");
+const inputaddress = document.getElementById("#p-address");
+const card = document.getElementById("#div-user-card");
+
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+  inputface.src = resp.picture.large;
+  inputname.innerText = resp.results.name;
 }
+
+
+callApi();
